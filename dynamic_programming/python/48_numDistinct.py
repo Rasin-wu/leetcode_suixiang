727f728157b95d62468aaffbbbe427ba0f32a7e3
skywuxuan <31404647+skywuxuan@users.noreply.github.com>
2023-05-30 21:14:43 +0800

    add 48_numDistinct.py in dynamic algorithm

---
+++ dynamic_programming/python/48_numDistinct.py
@@ -0,0 +1,40 @@
+#!/mnt/lustre02/jiangsu/aispeech/home/xmw01/tools/anaconda3/bin/python
+# -*- coding: UTF-8 -*-
+
+from typing import List
+
+"""
+给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。
+
+字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。（例如，"ACE" 是 "ABCDE" 的一个子序列，而 "AEC" 不是）
+
+题目数据保证答案符合 32 位带符号整数范围。
+"""
+
+# dp[i][j]：以i-1为结尾的s子序列中出现以j-1为结尾的t的个数为dp[i][j]。
+# 还没理解
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+        for i in range(len(s)):
+            dp[i][0] = 1
+
+        for j in range(1, len(t)):
+            dp[0][j] = 0
+
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return dp[-1][-1]
+    
+    
+if __name__ == '__main__':
+    s = "babgbag"
+    t = "bag"
+
+    solution = Solution()
+    result = solution.numDistinct(s, t)
+    print(result)
