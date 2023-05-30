e7f20d057406d663247472a1b2cfa79579918964
skywuxuan <31404647+skywuxuan@users.noreply.github.com>
2023-05-30 23:06:28 +0800

    add 49_minDistance.py in dynamic algorithm

---
+++ dynamic_programming/python/49_minDistance.py
@@ -0,0 +1,36 @@
+#!/mnt/lustre02/jiangsu/aispeech/home/xmw01/tools/anaconda3/bin/python
+# -*- coding: UTF-8 -*-
+
+from typing import List
+
+"""
+给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+
+示例：
+
+输入: "sea", "eat"
+输出: 2
+解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+"""
+#dp[i][j]：以i-1为结尾的字符串word1，和以j-1位结尾的字符串word2，想要达到相等，所需要删除元素的最少次数。
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0] * (len(word2) + 1) for _ in range(len(word2) + 1)]
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for j in range(len(word2) + 1):
+            dp[0][j] = j
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i][j - 1] + 1, dp[i - 1][j] + 1, dp[i - 1][j - 1] + 2)
+        return dp[-1][-1]
+
+if __name__ == '__main__':
+    word1 = "sea"
+    word2 = "eat"
+    solution = Solution()            
+    result = solution.minDistance(word1, word2)
+    print(result)
