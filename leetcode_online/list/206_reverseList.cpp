e1cc6d281b3c6d787369006aad840d2625735e08
skywuxuan <31404647+skywuxuan@users.noreply.github.com>
2023-06-16 16:53:34 +0800

    add 206_reverseList.cpp in leetcode_online/list

---
+++ leetcode_online/list/206_reverseList.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************
+> File Name:     206_reverseList.cpp
+> Author:        xmw01
+> Created Time:  Thu 15 Jun 2023 08:34:57 PM CST
+> Description:   
+ ************************************************************************/
+
+/*
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+https://leetcode.cn/problems/reverse-linked-list/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        
+        while (cur) {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
