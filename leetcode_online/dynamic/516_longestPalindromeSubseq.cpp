d0219ad0ed68733ad72e19eec7e0a5f35dc08703
skywuxuan <31404647+skywuxuan@users.noreply.github.com>
2023-06-26 19:58:57 +0800

    add 516_longestPalindromeSubseq.cpp in leetcode_online/dynamic

---
+++ leetcode_online/dynamic/516_longestPalindromeSubseq.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************
+> File Name:     516_longestPalindromeSubseq.cpp
+> Author:        xmw01
+> Created Time:  Mon 26 Jun 2023 07:57:47 PM CST
+> Description:   
+ ************************************************************************/
+
+/*
+给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+
+子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+
+输入：s = "bbbab"
+输出：4
+解释：一个可能的最长回文子序列为 "bbbb" 。
+*/
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+        for (int i = 0; i < s.size(); i++) dp[i][i] = 1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.size() - 1];
+    }
+};
